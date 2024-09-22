7084cd1388e0e8aeed26ef65b78dd72231eb7f4a
Tejas-78 <jadhavjee0@gmail.com>
2024-09-22 19:20:44 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import server from './backend/server.js';
-import {connect} from '../BuzzLine/backend/config.js'
+import {connect} from './backend/config.js'
 
 server.listen(3000,()=>{
     console.log('server listen on 3000')
